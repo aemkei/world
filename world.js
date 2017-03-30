787be1ebf28bec0e45e8629b4660ecf04d62074a
Martin Kleppe <kleppe@ubilabs.net>
2017-03-30 22:48:10 +0200

    quick version that runs in Node.js

--- world.js
+++ world.js
@@ -1,16 +1,15 @@
-eval(z='p="<"+"pre>"/* ,.oq#+     ,._, */;for(y in n="zw24l6k\
+eval(z='p=""        /* ,.oq#+     ,._, */;for(y in n="zw24l6k\
 4e3t4jnt4qj24xh2 x/* =<,m#F^    A W###q. */42kty24wrt413n243n\
 9h243pdxt41csb yz/* #K       q##H######Am */43iyb6k43pk7243nm\
 r24".split(4)){/* dP      cpq#q##########b, */for(a in t=pars\
 eInt(n[y],36)+/*         p##@###YG=[#######y */(e=x=r=[]))for\
 (r=!r,i=0;t[a/*         d#qg `*PWo##q#######D */]>i;i+=.05)wi\
-th(Math)x-= /*        aem1k.com Q###KWR#### W[ */.05,0>cos(o=\
-new Date/1e3/*      .Q#########Md#.###OP  A@ , */+x/PI)&&(e[~\
+th(Math)x-= /*        aem1k.com Q###KWR#### W[ */.05,0<cos(o=\
+new Date/1e3/*      .Q#########Md#.###OP  A@ , */-x/PI)&&(e[~\
 ~(32*sin(o)*/* ,    (W#####Xx######.P^     T % */sin(.5+y/7))\
 +60] =-~ r);/* #y    `^TqW####P###BP           */for(x=0;122>\
 x;)p+="   *#"/* b.        OQ####x#K           */[e[x++]+e[x++\
 ]]||(S=("eval"/* l         `X#####D  ,       */+"(z=\'"+z.spl\
 it(B = "\\\\")./*           G####B" #       */join(B+B).split\
 (Q="\'").join(B+Q/*          VQBP`        */)+Q+")//m1k")[x/2\
-+61*y-1]).fontcolor/*         TP         */(/\\w/.test(S)&&"#\
-03B");document.body.innerHTML=p+=B+"\\n"}setTimeout(z)')//
++61*y-1])+""/*         TP         */;p+=B+"\\n"}setTimeout(_=>eval(z),console.log(p))')//
